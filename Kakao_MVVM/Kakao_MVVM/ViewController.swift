6957fb674daf45a6154b711b20a7c5b8851510fb
유서린 <sprout3082@gmail.com>
2022-10-21 16:02:45 +0900

    [FEA] : MVVM Project 생성

---
+++ Kakao_MVVM/Kakao_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kakao_MVVM
+//
+//  Created by Mac Book Pro on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
